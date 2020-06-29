84e98b1f8d01d9bb0759a827afc9255ac4a5bd76
fengaihua0212 <fengaihua0212@gmail.com>
2020-06-29 16:32:15 +0800

    microservice-simple-provider-user

---
+++ microservice-simple-provider-user/src/main/java/com/fah/spring/cloud/study/entity/User.java
@@ -0,0 +1,30 @@
+package com.fah.spring.cloud.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author Administrator
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String userName;
+    @Column
+    private String name;
+    @Column
+    private Integer age;
+    @Column
+    private BigDecimal balance;
+}
---
+++ microservice-simple-provider-user/src/test/java/com/fah/spring/cloud/study/ProviderUserApplicationTest.java
@@ -0,0 +1,7 @@
+package com.fah.spring.cloud.study;
+
+import junit.framework.TestCase;
+
+public class ProviderUserApplicationTest extends TestCase {
+
+}
