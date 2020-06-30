c2748d8f745425426b19ee59ff6c5ad6ecc98ad8
fengaihua0212 <fengaihua0212@gmail.com>
2020-06-30 14:04:07 +0800

    服务注册与服务发现-Eureka

---
+++ microservice-discovery-eureka/src/test/java/com/fah/spring/cloud/study/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fah.spring.cloud.study;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
